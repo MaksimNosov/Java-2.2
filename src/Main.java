ec3c1563e16ad7f23f674787ef02d0bed6ac6a7d
MaksimNosov <m.s.nosov@gmail.com>
2022-07-14 14:33:19 +0300

    Small Letter

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        int InitAccount = 70; // исходный баланс счета
+        int initAccount = 70; // исходный баланс счета
         int deposit = 1198; // сумма пополнения
-        int DepositForBonus = 1000; // сумма, больше которой нужно пополнить, чтобы получить бонус
-        int DepositFor1Bonus = 100; // количество рублей пополнения для 1 рубля бонуса
-        int MinBonus = 1; // количество бонусов за пополнение на каждые 100 рублей
+        int depositForBonus = 1000; // сумма, больше которой нужно пополнить, чтобы получить бонус
+        int depositFor1Bonus = 100; // количество рублей пополнения для 1 рубля бонуса
+        int minBonus = 1; // количество бонусов за пополнение на каждые 100 рублей
 
-        if (deposit > DepositForBonus) {
-            MinBonus = 1;
+        if (deposit > depositForBonus) {
+            minBonus = 1;
         } else {
-            MinBonus = 0;
+            minBonus = 0;
         }
 
-        int Bonus = deposit / DepositFor1Bonus * MinBonus;
-        int TotalAccount = InitAccount + deposit + Bonus;
+        int bonus = deposit / depositFor1Bonus * minBonus;
+        int totalAccount = initAccount + deposit + bonus;
 
-        System.out.println("Итоговый счет: " + TotalAccount + "; Количество бонусных рублей: " + Bonus);
+        System.out.println("Итоговый счет: " + totalAccount + "; Количество бонусных рублей: " + bonus);
     }
 }
 
